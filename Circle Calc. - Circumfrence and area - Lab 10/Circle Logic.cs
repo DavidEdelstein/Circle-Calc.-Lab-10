6abaf6db8aba2cb66485efc9601ce7772ecc618d
David Edelstein <David Edelstein>
2018-11-12 01:56:30 -0500

    1st commit,not finished

---
+++ Circle Calc. - Circumfrence and area - Lab 10/Circle Logic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Calc.__Circumfrence_and_area___Lab_10
+{
+    class Circle
+    {
+        private double Radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+
+        public double CalculateCircumference()
+        {
+           return 2 * Math.PI * Radius;
+        }
+        //public string CalculateFormattedCircumference()
+        //{
+            
+        //}
+        public double CalculateArea()
+        {
+            return Math.PI * (2 * Radius);
+        }
+        //public string CalculateFomattedArea()
+        //{
+
+        //}
+        //private string FormattedNumber(double x)
+        //{
+
+        //}
+    }
+}
---
+++ Circle Calc. - Circumfrence and area - Lab 10/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Calc.__Circumfrence_and_area___Lab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CircleCalculations();
+        }
+
+
+
+        private static void CircleCalculations()
+        {
+            while (true)
+            {
+                Console.WriteLine("What is the radius of your circle?");
+                if (int.TryParse(Console.ReadLine(), out int radius))
+                {
+                    Circle circle = new Circle(radius);
+                    Console.WriteLine("CIRCUMFERENCE:" + circle.CalculateCircumference());
+                    Console.WriteLine("-------------");
+                    Console.WriteLine("AREA:" + circle.CalculateArea());
+                    Console.WriteLine("=============");
+
+                    while (true)
+                    {
+                        Console.Write("Do you want to continue?(y/n)");
+                        string answer = Console.ReadLine();
+                        if (answer == "n")
+                        {
+                            return;
+                        }
+                        else if (answer == "y")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input--either y to continue or n to exit:");
+
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Only input whole numbers:");
+                }
+            }
+        }
+    }
+}
